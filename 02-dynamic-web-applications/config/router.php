fa41f5de5d576f5f0966e81a3cbee3d871ea6fa7
leonel <leonel@DESKTOP-0SDEIO0>
2024-09-03 20:45:09 -0300

    feat: add ability for site to handle 404 error

--- 02-dynamic-web-applications/config/router.php
+++ 02-dynamic-web-applications/config/router.php
@@ -10,4 +10,13 @@ $routes = [
 
 if (array_key_exists($route, $routes)) {
     require_once($routes[$route]);
+} else {
+    abort(404);
+}
+
+function abort($code)
+{
+    http_response_code($code);
+    require_once("views/{$code}.view.php");
+    die();
 }
