e77fb2da9330717e656925ed59ed6167599d6f8c
leonel <leonel@DESKTOP-0SDEIO0>
2024-09-03 20:11:03 -0300

    chore: separate views from logic

--- 02-dynamic-web-applications/about.php
+++ 02-dynamic-web-applications/about.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My PHP Site</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="font-mono flex flex-col h-screen">
-    <nav class="bg-gray-200 py-6 px-6 space-x-4 bg-pink-600">
-        <a href="/index.php" class="text-white font-bold">Home</a>
-        <a href="/about.php" class="text-white font-bold">About</a>
-        <a href="/contact.php" class="text-white font-bold">Contact</a>
-    </nav>
-
-    <main class="flex items-center justify-center h-full bg-pink-400">
-        <h1 class="text-white font-bold text-2xl">This is the About Page</h1>
-    </main>
-</body>
-
-</html>
+require_once "views/about.view.php";
