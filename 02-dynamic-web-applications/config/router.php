0c1196ee29b01a845f9a618ecc3a38f859f2f4c3
leonel <leonel@DESKTOP-0SDEIO0>
2024-09-03 20:48:30 -0300

    ref: refactor router by wrapping it in a function

--- 02-dynamic-web-applications/config/router.php
+++ 02-dynamic-web-applications/config/router.php
@@ -8,15 +8,20 @@ $routes = [
     '/contact' => 'controllers/contact.php',
 ];
 
-if (array_key_exists($route, $routes)) {
-    require_once($routes[$route]);
-} else {
-    abort(404);
-}
-
 function abort($code)
 {
     http_response_code($code);
     require_once("views/{$code}.view.php");
     die();
 }
+
+function mapRouteToControllerOrAbort($route, $routes)
+{
+    if (array_key_exists($route, $routes)) {
+        require_once($routes[$route]);
+    } else {
+        abort(404);
+    }
+}
+
+mapRouteToControllerOrAbort($route, $routes);
