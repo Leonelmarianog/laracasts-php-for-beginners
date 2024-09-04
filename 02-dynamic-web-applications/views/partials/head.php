37401eda71819a044fd416f9d8c01aefc856224f
leonel <leonel@DESKTOP-0SDEIO0>
2024-09-03 22:00:08 -0300

    chore: separate contents into partials

---
+++ 02-dynamic-web-applications/views/partials/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Site</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="font-mono flex flex-col h-screen">
